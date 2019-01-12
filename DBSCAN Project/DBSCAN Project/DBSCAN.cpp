7a41802f0af5818e8e52e3ad9d27699d1cb2f8da
Arkady Koretsky <arkady1992@gmail.com>
2019-01-13 00:27:23 +0200

    Finished the algorithm. What's left is to build the main and check it.

--- DBSCAN Project/DBSCAN Project/DBSCAN.cpp
+++ DBSCAN Project/DBSCAN Project/DBSCAN.cpp
@@ -1,24 +1,28 @@
 #include <iostream>
-#include <vector>
 #include <cmath>
+#include <set>
 
 using namespace std;
 
+/*
 #define noise -1;
 #define unclassified 0;
+*/
+
+const int noise = -1;
+const int unclassified = 0;
 
 class Point
 {
 	double x, y;
+	int label; // what cluster in belongs to
 
 public:
-	int clusterID;
-
 	double distance(Point& point)
 	{
 		double xDistance = this->x - point.x;
 		double yDistance = this->y - point.y;
-		return sqrt(xDistance*xDistance + yDistance * yDistance);
+		return sqrt(xDistance * xDistance + yDistance * yDistance);
 	}
 
 	// constructors
@@ -26,9 +30,76 @@ public:
 	{
 		this->x = x;
 		this->y = y;
-		this->clusterID = 0;
+		label = unclassified;
 	}
 
 	// destructors
-	~Point(){}
+	~Point() {}
+
+	// getters and setters
+	int getLabel() { return label; }
+	void setLabel(int label) { this->label = label; }
+};
+
+class DBSCAN
+{
+	set<Point> setOfPoints;
+	double epsilon;
+	int minimumAmountOfPoints;
+	int amountOfClusters;
+
+	// constructors
+	DBSCAN(set<Point> setOfPoints, double epsilon, int minimumAmountOfPoints)
+	{
+		this->setOfPoints = setOfPoints;
+		this->epsilon = epsilon;
+		this->minimumAmountOfPoints = minimumAmountOfPoints;
+		amountOfClusters = 0;
+	}
+
+	// destructors
+	~DBSCAN()
+	{
+
+	}
+
+	void clusterAlgorithm()
+	{
+		for (Point point : setOfPoints)
+		{
+			if (point.getLabel() != unclassified)
+				continue;
+			set<Point> neighbors = rangeQuery(point);
+			if (neighbors.size() < minimumAmountOfPoints)
+			{
+				point.setLabel(noise);
+				continue;
+			}
+			amountOfClusters++;
+			point.setLabel(amountOfClusters);
+			set<Point> seed = neighbors;
+			seed.erase(point);
+			for (Point point : seed)
+			{
+				if (point.getLabel() == noise)
+					point.setLabel(amountOfClusters);
+				if (point.getLabel() != unclassified)
+					continue;
+				point.setLabel(amountOfClusters);
+				neighbors = rangeQuery(point);
+				if (neighbors.size() >= minimumAmountOfPoints)
+					for (Point point : neighbors)
+						seed.insert(point);
+			}
+		}
+	}
+
+	set<Point> rangeQuery(Point& currentPoint)
+	{
+		set<Point> neighbors;
+		for (Point point : setOfPoints)
+			if (currentPoint.distance(point) <= epsilon)
+				neighbors.insert(point);
+		return neighbors;
+	}
 };
